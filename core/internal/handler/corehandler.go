f28f871506755dd1016ac51cfba7737cdab963df
YfNightWind <linyuhong.personal@outlook.com>
2022-10-26 00:32:12 +0800

    feat: 初始化go-zero

---
+++ core/internal/handler/corehandler.go
@@ -0,0 +1,28 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/zeromicro/go-zero/rest/httpx"
+	"light-cloud/src/core/internal/logic"
+	"light-cloud/src/core/internal/svc"
+	"light-cloud/src/core/internal/types"
+)
+
+func CoreHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.Request
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.Error(w, err)
+			return
+		}
+
+		l := logic.NewCoreLogic(r.Context(), svcCtx)
+		resp, err := l.Core(&req)
+		if err != nil {
+			httpx.Error(w, err)
+		} else {
+			httpx.OkJson(w, resp)
+		}
+	}
+}
---
+++ core/internal/logic/corelogic.go
@@ -0,0 +1,31 @@
+package logic
+
+import (
+	"context"
+
+	"light-cloud/src/core/internal/svc"
+	"light-cloud/src/core/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type CoreLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewCoreLogic(ctx context.Context, svcCtx *svc.ServiceContext) *CoreLogic {
+	return &CoreLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *CoreLogic) Core(req *types.Request) (resp *types.Response, err error) {
+	resp = new(types.Response)
+	resp.Message = "Hello World"
+
+	return
+}
