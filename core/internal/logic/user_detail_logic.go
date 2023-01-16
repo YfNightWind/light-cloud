e3d2532a1c2cd63e18426aede518c9786e675285
YfNightWind <linyuhong.personal@outlook.com>
2023-01-16 21:05:16 +0800

    feat: 完成用户详细

---
+++ core/internal/logic/user_detail_logic.go
@@ -0,0 +1,47 @@
+package logic
+
+import (
+	"context"
+	"errors"
+	"light-cloud/src/core/model"
+
+	"light-cloud/src/core/internal/svc"
+	"light-cloud/src/core/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type UserDetailLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewUserDetailLogic(ctx context.Context, svcCtx *svc.ServiceContext) *UserDetailLogic {
+	return &UserDetailLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *UserDetailLogic) UserDetail(req *types.UserDetailRequest) (resp *types.UserDetailResponse, err error) {
+	resp = &types.UserDetailResponse{}
+	userInfo := new(model.UserInfo)
+
+	get, err := model.Engine.Where("identity = ? ", req.Identity).Get(userInfo)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if !get {
+		return nil, errors.New("user not found")
+	}
+
+	// 如果存在
+	resp.Name = userInfo.Name
+	resp.Email = userInfo.Email
+
+	return
+}
