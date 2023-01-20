388163e8a90075c87f9bd4c428f061b1acd03f47
YfNightWind <linyuhong.personal@outlook.com>
2023-01-20 22:11:16 +0800

    🪷feat: 增加了一些定义字段

--- core/define/define.go
+++ core/define/define.go
@@ -12,7 +12,20 @@ type UserClaim struct {
 // JwtSignature jwt签名
 var JwtSignature = "你的JWT签名"
 
+// 邮箱的账号和授权码
 var (
 	MailUsername = "你发送的邮箱"
 	MailPassword = "你邮箱的授权码"
+)
+
+// 验证码相关
+var (
+	CodeLength = 6
+	ExpireTime = 300 // 秒
+)
+
+// redis
+var (
+	RedisAddress  = "你的redis地址"
+	RedisPassword = "你的redis密码，没有就空着"
 )
