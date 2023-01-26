4435cc59db82a302fff10f497d5b7436b510517c
YfNightWind <linyuhong.personal@outlook.com>
2023-01-26 19:34:18 +0800

    🥕feat: 初步完成文件名称修改

---
+++ core/internal/logic/user_file_name_update_logic.go
@@ -0,0 +1,34 @@
+package logic
+
+import (
+	"context"
+	"light-cloud/src/core/model"
+
+	"light-cloud/src/core/internal/svc"
+	"light-cloud/src/core/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type UserFileNameUpdateLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewUserFileNameUpdateLogic(ctx context.Context, svcCtx *svc.ServiceContext) *UserFileNameUpdateLogic {
+	return &UserFileNameUpdateLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *UserFileNameUpdateLogic) UserFileNameUpdate(req *types.UserFileNameUpdateRequest, userIdentity string) (resp *types.UserFileNameUpdateResponse, err error) {
+	data := model.UserRepository{Name: req.Name}
+	_, err = l.svcCtx.SQL.Where("identity = ? AND user_identity = ? ", req.Identity, userIdentity).Update(data)
+	if err != nil {
+		return
+	}
+	return
+}
