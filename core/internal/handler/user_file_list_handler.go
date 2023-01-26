9bf2fafb7954f3291e7683d92c9ee6f69f3f05cc
YfNightWind <linyuhong.personal@outlook.com>
2023-01-26 19:04:14 +0800

    🥐feat: 初步完成用户文件列表

---
+++ core/internal/handler/user_file_list_handler.go
@@ -0,0 +1,28 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/zeromicro/go-zero/rest/httpx"
+	"light-cloud/src/core/internal/logic"
+	"light-cloud/src/core/internal/svc"
+	"light-cloud/src/core/internal/types"
+)
+
+func UserFileListHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.UserFileListRequest
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+			return
+		}
+
+		l := logic.NewUserFileListLogic(r.Context(), svcCtx)
+		resp, err := l.UserFileList(&req, r.Header.Get("UserIdentity"))
+		if err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+		} else {
+			httpx.OkJsonCtx(r.Context(), w, resp)
+		}
+	}
+}
---
+++ core/internal/logic/user_file_list_logic.go
@@ -0,0 +1,69 @@
+package logic
+
+import (
+	"context"
+	"light-cloud/src/core/define"
+	"light-cloud/src/core/internal/svc"
+	"light-cloud/src/core/internal/types"
+	"light-cloud/src/core/model"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type UserFileListLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewUserFileListLogic(ctx context.Context, svcCtx *svc.ServiceContext) *UserFileListLogic {
+	return &UserFileListLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *UserFileListLogic) UserFileList(req *types.UserFileListRequest, userIdentity string) (resp *types.UserFileListResponse, err error) {
+	resp = new(types.UserFileListResponse)
+	userFile := make([]*types.UserFile, 0)
+	deletedFile := make([]*types.DeletedUserFile, 0)
+
+	// 分页参数
+	size := req.Size
+	if size == 0 {
+		size = define.PageSize
+	}
+
+	page := req.Page
+	if page == 0 {
+		page = 1
+	}
+
+	offset := (page - 1) * size
+
+	// 查询用户文件列表
+	err = l.svcCtx.Engine.
+		Table("user_repository").
+		Where("user_identity = ?", req.Id, userIdentity).
+		Select("user_repository.id, user_repository.identity, user_repository.repository_identity, user_repository.ext, "+
+			"user_repository.name, repository_pool.path, repository_pool.size").
+		Join("LEFT", "repository_pool", "user_repository.repository_identity = repository_pool.identity").
+		Limit(size, offset).
+		Find(&userFile)
+	if err != nil {
+		return
+	}
+
+	// 查询总数
+	count, err := l.svcCtx.Engine.
+		Where("parent_id = ? AND user_identity = ? ", req.Id, userIdentity).Count(new(model.UserRepository))
+	if err != nil {
+		return
+	}
+
+	resp.List = userFile
+	resp.DeletedList = deletedFile
+	resp.Count = count
+	return
+}
