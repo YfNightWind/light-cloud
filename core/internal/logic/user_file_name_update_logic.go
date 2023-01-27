e1c63e58d4274aba0f24d55f9bf58695b575aa52
YfNightWind <linyuhong.personal@outlook.com>
2023-01-27 16:33:28 +0800

    🧀fix: 添加同级文件下名称重复判断

--- core/internal/logic/user_file_name_update_logic.go
+++ core/internal/logic/user_file_name_update_logic.go
@@ -2,6 +2,7 @@ package logic
 
 import (
 	"context"
+	"errors"
 	"light-cloud/src/core/model"
 
 	"light-cloud/src/core/internal/svc"
@@ -25,6 +26,16 @@ func NewUserFileNameUpdateLogic(ctx context.Context, svcCtx *svc.ServiceContext)
 }
 
 func (l *UserFileNameUpdateLogic) UserFileNameUpdate(req *types.UserFileNameUpdateRequest, userIdentity string) (resp *types.UserFileNameUpdateResponse, err error) {
+	// 判断该名称在当前文件夹下是否存在
+	count, err := l.svcCtx.SQL.Where("name = ? AND parent_id = (SELECT parent_id FROM user_repository as ur WHERE ur.identity = ? )", req.Name, req.Identity).
+		Count(new(model.UserRepository))
+	if err != nil {
+		return nil, err
+	}
+	if count > 0 {
+		return nil, errors.New("该文件名称已存在! ")
+	}
+	// 文件名称修改
 	data := model.UserRepository{Name: req.Name}
 	_, err = l.svcCtx.SQL.Where("identity = ? AND user_identity = ? ", req.Identity, userIdentity).Update(data)
 	if err != nil {
