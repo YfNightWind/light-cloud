e2c092f9ba8ed197a66e98a0af936df44da30c85
YfNightWind <linyuhong.personal@outlook.com>
2023-01-28 13:13:47 +0800

    🌽feat: 初步完成文件删除接口

---
+++ core/internal/logic/user_file_delete_logic.go
@@ -0,0 +1,35 @@
+package logic
+
+import (
+	"context"
+	"light-cloud/src/core/internal/svc"
+	"light-cloud/src/core/internal/types"
+	"light-cloud/src/core/model"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type UserFileDeleteLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewUserFileDeleteLogic(ctx context.Context, svcCtx *svc.ServiceContext) *UserFileDeleteLogic {
+	return &UserFileDeleteLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *UserFileDeleteLogic) UserFileDelete(req *types.UserFileDeleteRequest, userIdentity string) (resp *types.UserFileDeleteResponse, err error) {
+	_, err = l.svcCtx.SQL.Where("user_identity = ? AND identity = ? ", userIdentity, req.Identity).Delete(new(model.UserRepository))
+	if err != nil {
+		resp.Msg = "删除出错"
+		return
+	}
+	resp = new(types.UserFileDeleteResponse)
+	resp.Msg = "删除成功"
+	return
+}
