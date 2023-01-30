80cb7664429200203fc4a52a4bfc89d7ea1fb9c5
YfNightWind <linyuhong.personal@outlook.com>
2023-01-30 23:14:30 +0800

    🎯refactor: 将七牛云切换到腾讯云

--- core/helper/cos_upload.go
+++ core/helper/cos_upload.go
@@ -2,46 +2,39 @@ package helper
 
 import (
 	"context"
-	"github.com/qiniu/go-sdk/v7/auth/qbox"
-	"github.com/qiniu/go-sdk/v7/storage"
+	"github.com/tencentyun/cos-go-sdk-v5"
 	"light-cloud/src/core/define"
 	"net/http"
+	"net/url"
 	"path"
 )
 
-// CosUpload 上传文件到七牛云
-func CosUpload(r *http.Request, fileSize int64) (string, error) {
-	bucket := define.Bucket
+// CosUpload 上传文件到腾讯云
+func CosUpload(r *http.Request) (string, error) {
+	u, _ := url.Parse(define.CosBucket)
+	b := &cos.BaseURL{BucketURL: u}
+	client := cos.NewClient(b, &http.Client{
+		Transport: &cos.AuthorizationTransport{
+			SecretID:  define.TencentSecretID,
+			SecretKey: define.TencentSecretKey,
+		},
+	})
 
-	// 获取上传文件的对象
 	file, fileHeader, err := r.FormFile("file")
-
-	key := "light-cloud/" + UUID() + path.Ext(fileHeader.Filename)
-	putPolicy := storage.PutPolicy{
-		Scope: bucket,
-	}
-	mac := qbox.NewMac(define.AccessKey, define.SecretKey)
-	upToken := putPolicy.UploadToken(mac)
-	cfg := storage.Config{}
-
-	// 空间对应的机房
-	cfg.Region = &storage.ZoneHuadong
-
-	// 是否使用https域名
-	cfg.UseHTTPS = true
-
-	// 上传是否使用CDN上传加速
-	cfg.UseCdnDomains = true
-
-	// 构建表单上传的对象
-	formUploader := storage.NewFormUploader(&cfg)
-
-	ret := storage.PutRet{}
-
-	err = formUploader.Put(context.Background(), &ret, upToken, key, file, fileSize, nil)
 	if err != nil {
 		return "", err
 	}
 
-	return key, nil
+	fileName := UUID() + path.Ext(fileHeader.Filename)
+	key := "/" + fileName
+
+	_, err = client.Object.Put(
+		context.Background(), key, file, nil,
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	// 返回文件链接
+	return define.CosBucket + key, nil
 }
