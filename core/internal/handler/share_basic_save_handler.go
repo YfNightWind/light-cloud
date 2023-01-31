17238c347a424a3845c7721720389ed8c9dcfabe
YfNightWind <linyuhong.personal@outlook.com>
2023-01-31 15:31:37 +0800

    feat: 完成资源保存

---
+++ core/internal/handler/share_basic_save_handler.go
@@ -0,0 +1,28 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/zeromicro/go-zero/rest/httpx"
+	"light-cloud/src/core/internal/logic"
+	"light-cloud/src/core/internal/svc"
+	"light-cloud/src/core/internal/types"
+)
+
+func ShareBasicSaveHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.ShareBasicSaveRequest
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+			return
+		}
+
+		l := logic.NewShareBasicSaveLogic(r.Context(), svcCtx)
+		resp, err := l.ShareBasicSave(&req, r.Header.Get("UserIdentity"))
+		if err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+		} else {
+			httpx.OkJsonCtx(r.Context(), w, resp)
+		}
+	}
+}
---
+++ core/internal/logic/share_basic_save_logic.go
@@ -0,0 +1,61 @@
+package logic
+
+import (
+	"context"
+	"light-cloud/src/core/helper"
+	"light-cloud/src/core/internal/svc"
+	"light-cloud/src/core/internal/types"
+	"light-cloud/src/core/model"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type ShareBasicSaveLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewShareBasicSaveLogic(ctx context.Context, svcCtx *svc.ServiceContext) *ShareBasicSaveLogic {
+	return &ShareBasicSaveLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *ShareBasicSaveLogic) ShareBasicSave(req *types.ShareBasicSaveRequest, userIdentity string) (resp *types.ShareBasicSaveResponse, err error) {
+	resp = new(types.ShareBasicSaveResponse)
+	// 获取资源详情 from repository_pool
+	rp := new(model.RepositoryPool)
+	get, err := l.svcCtx.SQL.
+		Table("repository_pool").
+		Where("identity = ?", req.RepositoryIdentity).
+		Get(rp)
+	if err != nil {
+		resp.Msg = "error"
+		return
+	}
+	if !get {
+		resp.Msg = "资源不存在"
+		return
+	}
+
+	// user_repository 资源保存
+	usr := &model.UserRepository{
+		Identity:           helper.UUID(),
+		UserIdentity:       userIdentity,
+		ParentId:           req.ParentId,
+		RepositoryIdentity: req.RepositoryIdentity,
+		Ext:                rp.Ext,
+		Name:               rp.Name,
+	}
+	_, err = l.svcCtx.SQL.Insert(usr)
+	if err != nil {
+		resp.Msg = "save error"
+		return
+	}
+	resp.Identity = usr.Identity
+	resp.Msg = "success"
+	return
+}
