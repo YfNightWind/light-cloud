e12507b8f668095ff24e3e7b58672a0da0dad1c4
YfNightWind <linyuhong.personal@outlook.com>
2023-03-03 00:26:45 +0800

    feat: 增加查找已删除文件的SQL语句

--- core/internal/logic/user_file_list_logic.go
+++ core/internal/logic/user_file_list_logic.go
@@ -41,26 +41,46 @@ func (l *UserFileListLogic) UserFileList(req *types.UserFileListRequest, userIde
 		page = 1
 	}
 
-	offset := (page - 1) * size
+	//offset := (page - 1) * size
 
 	// 查询用户文件列表
 	//l.svcCtx.SQL.ShowSQL(true)
 	err = l.svcCtx.SQL.
 		Table("user_repository").
-		Where("parent_id = ? AND user_identity = ?", req.Id, userIdentity).
-		Select("user_repository.id, user_repository.identity, user_repository.repository_identity, user_repository.ext, "+
+		Select("user_repository.id, user_repository.parent_id, user_repository.identity, "+
+			"user_repository.repository_identity, user_repository.ext, user_repository.updated_at,"+
 			"user_repository.name, repository_pool.path, repository_pool.size").
-		Join("LEFT", "repository_pool", "user_repository.repository_identity = repository_pool.identity").
+		Where("user_identity = ?", userIdentity).
 		Where("user_repository.deleted_at = ? OR user_repository.deleted_at IS NULL", time.Time{}.Format(define.Datetime)).
-		Limit(size, offset).
+		Join("LEFT", "repository_pool", "user_repository.repository_identity = repository_pool.identity").
+		//Limit(size, offset).
 		Find(&userFile)
 	if err != nil {
+		resp.Msg = "error"
+		return
+	}
+
+	// 查询用户已删除文件
+	err = l.svcCtx.SQL.
+		Table("user_repository").
+		Select("user_repository.id, user_repository.parent_id, user_repository.identity, "+
+			"user_repository.repository_identity, user_repository.ext, user_repository.deleted_at,"+
+			"user_repository.name, repository_pool.path, repository_pool.size").
+		Where("user_identity = ? ", userIdentity).
+		Where("user_repository.deleted_at IS NOT NULL").
+		// Order("user_repository.deleted_at desc").
+		Join("LEFT", "repository_pool", "user_repository.repository_identity = repository_pool.identity").
+		Find(&deletedFile)
+
+	if err != nil {
+		resp.Msg = "error"
 		return
 	}
 
 	// 查询总数
 	count, err := l.svcCtx.SQL.
-		Where("parent_id = ? AND user_identity = ? ", req.Id, userIdentity).Count(new(model.UserRepository))
+		Where("parent_id = ? AND user_identity = ? AND deleted_at IS NULL", req.Id, userIdentity).
+		Count(new(model.UserRepository))
 	if err != nil {
 		return
 	}
@@ -68,5 +88,6 @@ func (l *UserFileListLogic) UserFileList(req *types.UserFileListRequest, userIde
 	resp.List = userFile
 	resp.DeletedList = deletedFile
 	resp.Count = count
+	resp.Msg = "success"
 	return
 }
